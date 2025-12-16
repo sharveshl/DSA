298c07f67c8f3184b35c35a1f93937c41e6b3ff6
Sharvesh <loganathansharvesh14@gmail.com>
2025-12-16 11:37:19 +0530

    method to check binary tree conditions

---
+++ BST/unique binary tree requirement.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+
+    public static boolean isPossible(int a, int b) {
+        // Code here
+        return (a==2 || b==2) && a!=b;
+    }
+}
