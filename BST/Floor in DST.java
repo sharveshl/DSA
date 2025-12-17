a9b46075fa51fad6310ca8cd84c4346db120912e
Sharvesh <loganathansharvesh14@gmail.com>
2025-12-17 11:21:44 +0530

    Add floor function for BST
    Implement floor function to find the largest value less than or equal to x in a BST.

---
+++ BST/Floor in DST.java
@@ -0,0 +1,30 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public static int floor(Node root, int x) {
+        // code here
+        int floor = -1;
+        while(root!=null){
+            if(root.data==x) 
+                return x;
+            else if(root.data<x){
+                floor = root.data;
+                root = root.right;
+            }
+            else
+                root = root.left;
+        }
+        return floor;
+    }
+}
