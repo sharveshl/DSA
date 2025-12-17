ed833327460a79bf32f20b3eebcfabba649d5536
Sharvesh <loganathansharvesh14@gmail.com>
2025-12-17 11:16:19 +0530

    Add findCeil method in Solution class
    Implement findCeil method to find the ceiling value in a BST.

---
+++ BST/Ceil in BST.java
@@ -0,0 +1,28 @@
+/* class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    int findCeil(Node root, int x) {
+        // code here
+        int ceil = -1;
+        while(root!=null){
+            if(root.data==x)
+                return x;
+            else if(x<root.data){
+                ceil = root.data;
+                root = root.left;
+            }
+            else{
+                root = root.right;
+            }
+        }
+        return ceil;
+    }
+}
